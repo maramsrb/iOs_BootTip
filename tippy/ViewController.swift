7fcd6b7979f8048900d0971bb509b5ecf534bbc9
maramsrb <30325742+maramsrb@users.noreply.github.com>
2017-08-09 00:44:26 -0700

    Initial checkIn

---
+++ tippy/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Maria De la Rosa on 7/29/17.
+//  Copyright © 2017 Maria De la Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tiplabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var TipControl: UISegmentedControl!
+    
+    //my slider
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var Percentlabel: UILabel!
+    @IBAction func ValueChange(_ sender: Any) {
+        
+        
+        let currentTip = tipSlider.value
+        Percentlabel.text = String(format: "%.2f%%",currentTip)
+        calculateTip(sender)
+    }
+    
+    
+    //end slider
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        
+          let tipPercentage = Double(tipSlider.value)/100
+        //let tipPercentages = [0.18, 0.20, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        //let tip = bill * tipPercentages[TipControl.selectedSegmentIndex]
+        let tip = bill * tipPercentage
+        let total = tip + bill
+        
+        tiplabel.text = String(format: "$%.2f", tip)
+        TotalLabel.text = String(format: "$%.2f", total)
+        
+    }
+}
+
